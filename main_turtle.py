2f1a7d577772c803fe52d9f3deced15d65ae773c
Mordor828 <tranm98@gmail.com>
2025-05-23 12:02:34 +0200

    updates

--- main_turtle.py
+++ main_turtle.py
@@ -5,7 +5,17 @@ import turtle
 wn = turtle.Screen()
 wn.bgcolor("lightgreen")
 
+#Create player turtle
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
 
+#Set speed variable
+speed = 1
+
+while True:
+    player.forward(speed)
 
 
 

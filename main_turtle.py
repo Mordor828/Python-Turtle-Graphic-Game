42deb523b3bb10ca74cfbd9c7debf811f8e4cee8
Mordor828 <tranm98@gmail.com>
2025-05-26 12:18:08 +0200

    #updates again...

--- main_turtle.py
+++ main_turtle.py
@@ -9,6 +9,12 @@ wn.bgcolor("lightgreen")
 mypen = turtle.Turtle()
 mypen.penup()
 mypen.setposition(-300,-300)
+mypen.pendown()
+mypen.pensize(3)
+for side in range(4):
+    mypen.forward(600)
+    mypen.left(90)
+mypen.hideturtle()
 
 
 #Create player turtle
